f7d9412bb2f6cae155fbea5a0f54917a88b591e1
ShoyebAkter <heroreal5385@gmail.com>
2022-03-25 20:50:38 -0700

    photos problem solve

--- src/components/Player/Player.js
+++ src/components/Player/Player.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 import "./Player.css"
 
-const Player = (props) => {
-    const {photo,age,name}=props.player;
+const Player = ({player,handleAddToCart}) => {
+    const {photo,age,name}=player;
+    console.log(photo)
     return (
         <div className='player'>
-            <img src={photo} alt=""></img>
-            <p>Name: {name}</p>
+            <img src="photos/messi.jpg" alt=""></img>
+            <div className='player-info'>
+            <p className='player-name'>Name: {name}</p>
             <p>Age: {age}</p>
+        </div>
+        <button onClick={() => handleAddToCart(player)} className='btn'>
+            <p className='btn-text'>Add to Cart</p>
+            <FontAwesomeIcon icon={faShoppingCart}></FontAwesomeIcon>
+        </button>
         </div>
     );
 };
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,15 +1,21 @@
 import React, { useEffect, useState } from 'react';
 import Player from '../Player/Player';
-import "../Shop/Shop.css";
+import "./Shop.css";
 
 const Shop = () => {
     const [players,setplayers]=useState([]);
+    const [cart, setCart] = useState([]);
 
     useEffect(()=>{
         fetch('products.json')
         .then(res=> res.json())
         .then(data=> setplayers(data))
     },[]);
+
+    const handleAddToCart = (product) =>{
+        const newCart = [...cart, product];
+        setCart(newCart);
+    }
     return (
         <div className='container'>
             <div className='players-container'>
@@ -17,11 +23,18 @@ const Shop = () => {
                     players.map(player=><Player
                     key={player.id}
                     player={player}
+                    handleAddToCart={handleAddToCart}
                     ></Player>)
                 }
             </div>
             <div className='players-cart'>
-                <p>This is cart</p>
+            <h4>Selected player</h4>
+                <div >
+                    <button className='choose'> Choose 1 player</button>
+                </div>
+                <div>
+                    <button className='reset'>Choose Again</button>
+                </div>
             </div>
         </div>
     );
